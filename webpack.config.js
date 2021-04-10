6776ab837d9d7f25fa80f32b89e999099a90fa05
sarahp <me@sarahp.studio>
2021-04-10 13:14:26 -0400

    Fixing css loader error

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
                 test: /\.css$/,
                 loader: 'css-loader',
                 query: {
-                    modules: true,
-                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                    importLoaders: 1,
+                    modules: true
                 }
             }
         ]
